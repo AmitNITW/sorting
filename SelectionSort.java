c8d7dcfdc35e96bdf75be12bf8b5c41435af61b9
Amit Kumar <amit.gupta894@gmail.com>
2018-07-20 07:28:06 +0530

    added stable selection sort.

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,43 @@
 
 public class SelectionSort {
 	/*
+	 * Selection sort works by finding the minimum element and then inserting it in
+	 * its correct position by swapping with the element which is in the position of
+	 * this minimum element. This is what makes it unstable. Swapping might impact
+	 * in pushing a key(let’s say A) to a position greater than the key(let’s say B)
+	 * which are equal keys. which makes them out of desired order. In the selection
+	 * sort 4A was pushed after 4B and after complete sorting this 4A remains after
+	 * this 4B. Hence resulting in unstability.
+	 * 
+	 * Input : 4A 5 3 2 4B 1 
+	 * Output : 1 2 3 4B 4A 5
+	 * 
+	 * Stable Selection Sort would have produced, Selection sort can be made Stable
+	 * if instead of swapping, the minimum element is placed in its position without
+	 * swapping
+	 * 
+	 * Output : 1 2 3 4A 4B 5
+	 */
+	public static void stableSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[min] > arr[j]) {
+					min = j;
+				}
+			}
+
+			int key = arr[min];
+
+			while (min > i) {
+				arr[min] = arr[min - 1];
+				min--;
+			}
+			arr[i] = key;
+		}
+	}
+	
+	/*
 	 * Recursion start
 	 */
 	public static void sortUsingRecursion(int[] arr) {
